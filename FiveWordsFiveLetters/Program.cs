fb08bd0d3f4cc977e97f1a635f8abbe02221bdbd
luca5393 <120323430+luca5393@users.noreply.github.com>
2024-10-16 10:15:07 +0200

    High Speed

--- FiveWordsFiveLetters/Program.cs
+++ FiveWordsFiveLetters/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Diagnostics;
 using System.Linq;
+using System.Text;
 
 namespace FiveWordsFiveLetters
 {
@@ -7,67 +8,91 @@ namespace FiveWordsFiveLetters
     {
         private static List<string> results = new List<string>();
         private static Dictionary<int, string> wordDictionary = new Dictionary<int, string>();
-        private static List<int> words = new List<int>();
+        private static int[] words;
+
+        private static int wordAmount = 5;
+        private static int wordLength = 5;
         static void Main(string[] args)
         {
+
+            List<int> tempWordList = new List<int>();
+
             Stopwatch sw = new Stopwatch();
+            Stopwatch ssw = new Stopwatch();
             sw.Start();
+            ssw.Start();
             Console.WriteLine("Starting cleaning");
             string[] lines = File.ReadAllLines(@"C:\\Users\\HFGF\\Documents\\GitHub\\H2\\FiveWordsFiveLetters\\Words.txt");
 
-
             foreach (string line in lines)
             {
-                if (line.Length == 5 && line.Distinct().Count() == 5)
+                if (line.Length == wordLength)
                 {
                     int bitmask = 0;
+                    bool unique = true;
                     foreach (char c in line)
                     {
+                        if ((bitmask & 1 << (c - 'a')) != 0)
+                        {
+                            unique = false;
+                            break;
+                        }
                         bitmask |= 1 << (c - 'a');
                     }
-                    words.Add(bitmask);
-                    wordDictionary.TryAdd(bitmask, line);
+
+                    if (unique)
+                    {
+                        tempWordList.Add(bitmask);
+                        wordDictionary.TryAdd(bitmask, line);
+                    }
                 }
             }
+            words = tempWordList.ToArray();
+            ssw.Stop();
             Console.WriteLine("Cleaning done");
+            Console.WriteLine($"Time for clean: {ssw.ElapsedMilliseconds}");
             HashSet<string> sentences = new HashSet<string>();
 
             Console.WriteLine("Starting Search");
-            findNext(new int[0], 0, 0);
+            int[] currentWords = new int[wordAmount];
+            findNext(currentWords, 0, 0, 0);
             Console.WriteLine("Search Done");
 
             sw.Stop();
 
-            
             foreach (string word in results) 
             {
                 Console.WriteLine(word);
             }
-            
+
             Console.WriteLine("Total count: " + results.Count);
-            Console.WriteLine("Total time: " + sw.ElapsedMilliseconds);
+            Console.WriteLine($"Total time: { sw.ElapsedMilliseconds}");
         }
 
-        static void findNext(int[] mask, int index, int fullmask)
+        static void findNext(int[] currentWords, int index, int count, int currentWordsAsOne)
         {
-            if (mask.Length == 5)
+            if (count == wordAmount)
             {
-                string result = string.Empty;
-                foreach (int word in mask)
-                {
-                    result += wordDictionary[word];
-                }
-                results.Add(result);
+
+                results.Add(string.Concat(currentWords.Select(word => wordDictionary[word])));
+                return;
+
+            }
+
+            if (words.Length - index < wordAmount - count)
+            {
                 return;
             }
 
-            for (int i = index; i < words.Count(); i++)
+            for (int i = index; i < words.Length; i++)
             {
-                if ((fullmask & words[i]) == 0)
+                if ((currentWordsAsOne & words[i]) == 0)
                 {
-                    findNext(mask.Append(words[i]).ToArray(), i+1, (fullmask | words[i]));
+                    currentWords[count] = words[i];
+                    findNext(currentWords, i + 1, count + 1, currentWordsAsOne | words[i]);
                 }
             }
+            
         }
     }
 }
--- FiveWordsFiveLetters/obj/Debug/net8.0/FiveWordsFiveLetters.AssemblyInfo.cs
+++ FiveWordsFiveLetters/obj/Debug/net8.0/FiveWordsFiveLetters.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FiveWordsFiveLetters")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3a3b320cbab7287e663a1b895b2c915e8f208b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a624234afdaf9210216f093b957b74555280a734")]
 [assembly: System.Reflection.AssemblyProductAttribute("FiveWordsFiveLetters")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FiveWordsFiveLetters")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
